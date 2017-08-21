348a9ad152bb0a0f79b629e96aeced4dbed4ec6e
Static <yl@tourongchangfu.com>
2017-08-21 09:51:35 +0800

    lombok

--- src/main/java/net/ssm/system/web/pojo/SysUser.java
+++ src/main/java/net/ssm/system/web/pojo/SysUser.java
@@ -1,40 +1,16 @@
 package net.ssm.system.web.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class SysUser {
-	private Long userid;
-	private String username;
-	private String password;
-	private String email;
-	private String mobile;
-	public Long getUserid() {
-		return userid;
-	}
-	public void setUserid(Long userid) {
-		this.userid = userid;
-	}
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getMobile() {
-		return mobile;
-	}
-	public void setMobile(String mobile) {
-		this.mobile = mobile;
-	}
+    private Long userid;
+    private String username;
+    private String password;
+    private String email;
+    private String mobile;
 }
