6feaa60715465b2163ae8b6455e3c5ee963aa737
yalunwang <shenxy@longjubank.com>
2017-11-14 19:27:07 +0800

    菜单树checkbox

--- src/main/java/net/ssm/system/web/controller/RoleMenuController.java
+++ src/main/java/net/ssm/system/web/controller/RoleMenuController.java
@@ -1,8 +1,20 @@
 package net.ssm.system.web.controller;
 
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@RequestMapping("menu")
 public class RoleMenuController {
-	
+    @RequestMapping("roleMenu")
+    public ModelAndView setRoleMenu() {
+        ModelAndView modelAndView = new ModelAndView();
+
+
+        modelAndView.setViewName("role/roleMenu");
+
+        return modelAndView;
+    }
 }
