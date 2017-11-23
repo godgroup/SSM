7213449c57c9f1842a539dd949fa18a7250e0566
yalunwang <yalunwang12@163.com>
2017-11-23 23:21:56 +0800

    增加统一日志

---
+++ src/main/java/net/ssm/core/LogAll.java
@@ -0,0 +1,28 @@
+package net.ssm.core;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by Administrator on 2017/11/23.
+ */
+public class LogAll implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+        System.out.println("我处理完这一次请求了，哈哈哈哈"+request.getRequestURI());
+    }
+}
